2da2b729a6a675d25418f99571eeda131fa4d85a
Jim Farrugia <the,jim.farrugia@gmail.com>
2019-05-01 22:31:06 +1000

    ran Create Locations_Users migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_01_054309) do
+ActiveRecord::Schema.define(version: 2019_05_01_121946) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,15 @@ ActiveRecord::Schema.define(version: 2019_05_01_054309) do
     t.string "country"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "locations_users", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "location_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["location_id"], name: "index_locations_users_on_location_id"
+    t.index ["user_id"], name: "index_locations_users_on_user_id"
   end
 
   create_table "product_orders", force: :cascade do |t|
@@ -65,6 +74,8 @@ ActiveRecord::Schema.define(version: 2019_05_01_054309) do
   end
 
   add_foreign_key "listings", "users"
+  add_foreign_key "locations_users", "locations"
+  add_foreign_key "locations_users", "users"
   add_foreign_key "product_orders", "listings"
   add_foreign_key "product_orders", "users"
   add_foreign_key "users", "locations"
