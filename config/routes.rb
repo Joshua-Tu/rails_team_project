e92e170d04157eec46bb2d970f66ff10450fb0de
Jim Farrugia <the,jim.farrugia@gmail.com>
2019-05-01 20:29:31 +1000

    resolved routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,16 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   devise_for :users
   # Root/Homepage
   get "/", to: "pages#home", as: "root"
 
-  get "/listings", to: "listings#index", as: "listings"
-  post ""
+  #get "/listings", to: "listings#index", as: "listings"
 
-  get "/listings/:id", to: "listings#show", as: "single_listing"
+  #get "/listings/:id", to: "listings#show", as: "single_listing"
 
-  get "/users/:id", to: "users#show", as: "user_profile"
+  #get "/users/:id", to: "users#show", as: "user_profile"
 
   # Listings C.R.U.D.
   # Transactions C.U.
 
-=======
->>>>>>> master
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
