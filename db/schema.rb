fc7386c71908d8800db56f9a269fc1481241ef14
Nathaniel  Morales <nathanielmorales01@gmail.com>
2019-05-01 11:47:16 +1000

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,14 @@ ActiveRecord::Schema.define(version: 2019_05_01_003955) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "locations", force: :cascade do |t|
+    t.string "city"
+    t.string "state"
+    t.string "country"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -25,14 +33,6 @@ ActiveRecord::Schema.define(version: 2019_05_01_003955) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-
-  create_table "locations", force: :cascade do |t|
-    t.string "city"
-    t.string "state"
-    t.string "country"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-
   end
 
 end
