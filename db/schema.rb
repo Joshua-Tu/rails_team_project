0b41a8305f7af5f04d018bac27b2e60cbb19cb59
Nathaniel  Morales <nathanielmorales01@gmail.com>
2019-05-01 14:24:23 +1000

    change price attribute from float to integer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_01_035358) do
+ActiveRecord::Schema.define(version: 2019_05_01_041807) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 2019_05_01_035358) do
     t.bigint "user_id"
     t.text "description"
     t.string "title"
-    t.float "price"
+    t.integer "price"
     t.boolean "show_phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
