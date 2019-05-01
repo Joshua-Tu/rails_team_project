5991d836a2806b97628bb5e5c94691cde69d59b7
Nathaniel  Morales <nathanielmorales01@gmail.com>
2019-05-01 11:59:04 +1000

    added user migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_01_003955) do
+ActiveRecord::Schema.define(version: 2019_05_01_015535) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,8 +31,14 @@ ActiveRecord::Schema.define(version: 2019_05_01_003955) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "full_name"
+    t.integer "phone_number"
+    t.string "username"
+    t.bigint "location_id"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["location_id"], name: "index_users_on_location_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "users", "locations"
 end
