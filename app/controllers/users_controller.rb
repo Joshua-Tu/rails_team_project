168f91ab69e06dbaa322c73cbf4908f3429a2ca9
Joshua-Tu <tjh8523@gmail.com>
2019-05-02 15:52:22 +1000

    added dynamic id to the end point

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,11 @@ class UsersController < ApplicationController
     @users = User.all
   end
  
-  # # GET /users/1
+  # # GET /users/:id
   # # GET /users/1.json
   def show
- 
+    @user = User.find(params[:id])
+    
   end
  
   # GET /users/1/edit
@@ -43,4 +44,4 @@ class UsersController < ApplicationController
  
 end
 
-end
+
