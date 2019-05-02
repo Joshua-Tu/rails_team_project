2ce543fcc4111b397a309cd6c4b06160368b0b5a
Nathaniel  Morales <nathanielmorales01@gmail.com>
2019-05-02 14:49:53 +1000

    resolving merge conflicts, rolling back 2 commits

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,48 +1,3 @@
-<<<<<<< HEAD
-class ListingsController < ActionController::Base
-    before_action :set_listings, only: [:show, :edit, :update, :destroy]
-    before_action :authenticate_user!, except: [:index, :show]
-
-
-
-    def index
-        @listings = Listing.all
-    end
-
-    def create
-        @listing = Listing.new(listing_params)
-
-        respond_to do |format|
-            if @listing.save
-              format.html { redirect_to @listing, notice: 'Article was successfully created.' }
-              format.json { render :show, status: :created, location: @listing }
-            else
-              format.html { render :new }
-              format.json { render json: @article.errors, status: :unprocessable_entity }
-            end
-    end
-
-    def new
-        @listing = Listing.new
-    end
-
-    def update
-    end
-
-
-    def show
-    end
-
-    def edit
-    end
-
-    def destroy
-    end
-
-    private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_listings
-=======
 class ListingsController < ApplicationController
   before_action :set_listing, only: [:show, :edit, :update, :destroy]
 
@@ -110,20 +65,11 @@ class ListingsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_listing
->>>>>>> dev
       @listing = Listing.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def listing_params
-<<<<<<< HEAD
-      params.require(:article).permit(:title, :description)
-    end
-end
-
-end
-=======
       params.fetch(:listing, {})
     end
 end
->>>>>>> dev
