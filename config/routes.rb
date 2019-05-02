205b6130d05ebed28469e467180310928c78a7f0
Joshua-Tu <tjh8523@gmail.com>
2019-05-02 15:18:37 +1000

    created users controller and editted routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :listings
+  resources :users, only[:index, :show, :edit, :update]
 
   # Root/Homepage
   get "/", to: "pages#home", as: "root"
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
