ee43cbe9fc767740fa3e0f4b06c08205b0aa0160
Nathaniel  Morales <nathanielmorales01@gmail.com>
2019-05-02 16:32:53 +1000

    add form field for price and description and views

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -72,6 +72,6 @@ class ListingsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def listing_params
-      params.require(:listing).permit(:title)
+      params.require(:listing).permit(:title, :price, :description)
     end
 end
