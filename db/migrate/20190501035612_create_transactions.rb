9216716a80b8c8d37d6a10a4f6926e10912c9dbd
Jim Farrugia <the,jim.farrugia@gmail.com>
2019-05-05 03:48:04 +1000

    restored create_transactions migration

--- db/migrate/20190501035612_create_transactions.rb
+++ db/migrate/20190501035612_create_transactions.rb
@@ -2,10 +2,11 @@ class CreateTransactions < ActiveRecord::Migration[5.2]
   def change
     create_table :transactions do |t|
       t.references :user, foreign_key: true
+      t.integer :price
       t.date :date
       t.references :listing, foreign_key: true
       t.text :comment
-      t.integer :rating
+      t.integer :score
 
       t.timestamps
     end
