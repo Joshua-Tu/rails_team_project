635a6949ffc0ab5c3311bd6a72231ffaaf7fe5f5
Jim Farrugia <the,jim.farrugia@gmail.com>
2019-05-05 15:06:14 +1000

    fixed @sold_listings instance variable

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   # # GET /users/1.json
   def show
     @active_listings = @user.listings.where(product_order: [nil, ""])
-    @sold_listings = @user.listings.where.not(buyer: [nil, ""])
+    @sold_listings = @user.listings.where.not(product_order: [nil, ""])
   end
   
   # # PATCH/PUT /users/1
