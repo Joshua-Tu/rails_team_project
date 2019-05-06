d19e1e9796dc90420530ab4e97912a2c5ea67454
Jim Farrugia <the,jim.farrugia@gmail.com>
2019-05-07 09:56:39 +1000

    generated migration to remove unused columns from ProductOrder

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_03_012206) do
+ActiveRecord::Schema.define(version: 2019_05_06_235323) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,11 +66,7 @@ ActiveRecord::Schema.define(version: 2019_05_03_012206) do
 
   create_table "product_orders", force: :cascade do |t|
     t.bigint "user_id"
-    t.integer "price"
-    t.date "date"
     t.bigint "listing_id"
-    t.text "comment"
-    t.integer "score"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["listing_id"], name: "index_product_orders_on_listing_id"
