439b89f88ccdde74cfa567a8a5c6cc614aa4e6c7
Jim Farrugia <the,jim.farrugia@gmail.com>
2019-05-06 10:45:59 +1000

    added highest price listing

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,9 @@ class PagesController < ApplicationController
         highest_price = Listing.maximum("price")
         # Find the first listing which has the highest price.
         @highest_priced_listing = Listing.find_by(price: highest_price)
-        p @highest_priced_listing
+
+        lowest_price = Listing.maximum("price")
+        # Find the first listing which has the lowest price.
+        @lowest_priced_listing = Listing.find_by(price: lowest_price)
     end
 end
