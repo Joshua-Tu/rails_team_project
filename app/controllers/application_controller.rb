b2e06d85b10b65adc986229680d6da95e7a5fd2c
Joshua-Tu <tjh8523@gmail.com>
2019-05-07 16:41:08 +1000

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,5 @@ class ApplicationController < ActionController::Base
       devise_parameter_sanitizer.permit(:sign_up, keys: [:full_name, :phone_number, :username, :picture])
       devise_parameter_sanitizer.permit(:account_update, keys: [:full_name, :phone_number, :username, :picture])
     end
+
 end
