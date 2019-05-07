8d0d693977455145ca6e77af7cfdc68ee164c506
Joshua-Tu <tjh8523@gmail.com>
2019-05-07 11:09:20 +1000

    changed logic and in user profile page's transanction history part

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,25 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_user, :set_users, :set_listings, only: [:show, :edit, :update, :index]
   before_action :authenticate_user!
 
   def index
-    @users = User.all
+
   end
  
   # # GET /users/:id
   # # GET /users/1.json
   def show
-    @active_listings = []
-    @sold_listings = []
-    @user.listings.each do |listing|
-      if listing.product_order
-        # Listing has a product_order
-        @sold_listings.push listing
-      else
-        # Listing doesn't have a product order
-        @active_listings.push listing
-      end
-    end
+    # @active_listings = []
+    # @sold_listings = []
+    # @user.listings.each do |listing|
+    #   if listing.product_order
+    #     # Listing has a product_order
+    #     @sold_listings.push listing
+    #   else
+    #     # Listing doesn't have a product order
+    #     @active_listings.push listing
+    #   end
+    # end
   end
   
   # # PATCH/PUT /users/1
@@ -40,6 +40,14 @@ class UsersController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_user
       @user = User.find(params[:id])
+    end
+
+    def set_users
+      @users = User.all
+    end
+
+    def set_listings
+      @listings = Listing.all
     end
  
     # Never trust parameters from the scary internet, only allow the white list through.
