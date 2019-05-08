b42815a2cd3e0bf3b0da8dd1af738721931c30ac
Nathaniel  Morales <nathanielmorales01@gmail.com>
2019-05-08 20:39:28 +1000

    complete crud test for listings CRUD

--- test/cypress/integration/signed_in_crud.js
+++ test/cypress/integration/signed_in_crud.js
@@ -17,27 +17,27 @@ describe('signed_in', function() {
         cy.contains('New Listing')
     });
 
-    // it("Title title less than 3", function() { 
-    //     cy.visit('/listings/new')
-    //     cy.contains('New Listing')
-    //     cy.get("[data-cy='title']").type("ab")
-    //     cy.get("[data-cy='price']").type("6")
-    //     cy.get("[data-cy='description']").type("this is a description")
-    //     cy.get("[data-cy='showphone']").click()
-    //     cy.get("[data-cy='submit']").click()
-    //     cy.contains("Title is too short (minimum is 3 characters)")
-    // });
+    it("Title title less than 3", function() { 
+        cy.visit('/listings/new')
+        cy.contains('New Listing')
+        cy.get("[data-cy='title']").type("ab")
+        cy.get("[data-cy='price']").type("6")
+        cy.get("[data-cy='description']").type("this is a description")
+        cy.get("[data-cy='showphone']").click()
+        cy.get("[data-cy='submit']").click()
+        cy.contains("Title is too short (minimum is 3 characters)")
+    });
 
-    // it("Title length greater than 50", function() { 
-    //     cy.visit('/listings/new')
-    //     cy.contains('New Listing')
-    //     cy.get("[data-cy='title']").type("aaaaabbbbbcccccdddddeeeeeaaaaabbbbbcccccdddddeeeeea")
-    //     cy.get("[data-cy='price']").type("6")
-    //     cy.get("[data-cy='description']").type("this is a description")
-    //     cy.get("[data-cy='showphone']").click()
-    //     cy.get("[data-cy='submit']").click()
-    //     cy.contains("Title is too long (maximum is 50 characters)")
-    // });
+    it("Title length greater than 50", function() { 
+        cy.visit('/listings/new')
+        cy.contains('New Listing')
+        cy.get("[data-cy='title']").type("aaaaabbbbbcccccdddddeeeeeaaaaabbbbbcccccdddddeeeeea")
+        cy.get("[data-cy='price']").type("6")
+        cy.get("[data-cy='description']").type("this is a description")
+        cy.get("[data-cy='showphone']").click()
+        cy.get("[data-cy='submit']").click()
+        cy.contains("Title is too long (maximum is 50 characters)")
+    });
 
     it("Price lower than 5", function() { 
         cy.visit('/listings/new')
@@ -48,6 +48,38 @@ describe('signed_in', function() {
         cy.get("[data-cy='showphone']").click()
         cy.get("[data-cy='submit']").click()
         cy.contains("New Listing")
+    });
+
+    it("Description is too short", function() { 
+        cy.visit('/listings/new')
+        cy.contains('New Listing')
+        cy.get("[data-cy='title']").type("Automated Listing Test")
+        cy.get("[data-cy='price']").type("6")
+        cy.get("[data-cy='description']").type("Success")
+        cy.get("[data-cy='showphone']").click()
+        cy.get("[data-cy='submit']").click()
+        cy.contains('Description is too short (minimum is 10 characters)')
+    });
+
+    it("Listing Created", function() { 
+        cy.visit('/listings/new')
+        cy.contains('New Listing')
+        cy.get("[data-cy='title']").type("Successful Listing Test")
+        cy.get("[data-cy='price']").type("6")
+        cy.get("[data-cy='description']").type("Listing Created")
+        cy.get("[data-cy='showphone']").click()
+        cy.get("[data-cy='submit']").click()
+        cy.contains('Listing was successfully created.')
+    });
+
+    it("Destroy user listed item", function() { 
+        cy.visit('/listings')
+        cy.get("[data-confirm='Are you sure?']").contains('Remove').click()
+        cy.wait(500)
+        cy.contains('Listing was successfully destroyed.')
+        // uncaught type error? message that a listing was successfully destroyed appears
+        
+
     });
 
 });
