28f583e02774bae8ad8458f607eaa0a0ef9c6096
Nathaniel  Morales <nathanielmorales01@gmail.com>
2019-05-08 14:07:16 +1000

    change tags to cypress

--- test/cypress/integration/users_registration.js
+++ test/cypress/integration/users_registration.js
@@ -3,8 +3,7 @@ describe('Users', function() {
 
     it('Show Sign in Page', function() {
         cy.visit('/users/sign_up') 
-        cy.get('form')
-        cy.get('.actions').children().contains('Sign up').click()
+
     });
 
     it('New user Sign Up', function() {
@@ -23,5 +22,22 @@ describe('Users', function() {
 
 
     });
+
+    // it('New user Sign Up', function() {
+    //     cy.visit('/users/sign_up')
+    //     cy.get('form').children()
+    //     cy.get("[data-cy='fullname']").type("automated name test")
+    //     cy.get("[data-cy='phonenumber']").type("4444666666")
+    //     cy.get("[data-cy='username']").type("Cypress Username")
+    //     cy.get("[data-cy='city']").type("City")
+    //     cy.get("[data-cy='state']").type("State")
+    //     cy.get("[data-cy='country']").type("Country")
+    //     cy.get("[data-cy='email']").type("cypress@test.com")
+    //     cy.get("[data-cy='password']").type("password")
+    //     cy.get("[data-cy='cpassword']").type("password")
+    //     cy.get("input[type='submit']").click()
+
+
+    // });
 
 });
