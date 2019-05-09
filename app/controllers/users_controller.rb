080d97e7ab2cfa0dcf9bb215b9db4ad06b2ed725
Nathaniel  Morales <nathanielmorales01@gmail.com>
2019-05-09 12:42:53 +1000

    fix the overlapping img in listing show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,37 @@
 class UsersController < ApplicationController
-  before_action :set_user, :set_users, :set_listings, only: [:index, :show, :edit, :update]
-  before_action :set_active_listings, :set_sold_listings, :set_purchased_listings, only: [:show]
+  before_action :set_user, only: [:show, :edit, :update]
   before_action :authenticate_user!
 
   def index
-
+    @users = User.all
   end
  
   # # GET /users/:id
   # # GET /users/1.json
   def show
-    # # private methods (bottom of this controller) give us access to:
-    # # @active_listings (user's listings that are for sale)
-    # # @sold_listings (listings the user has sold)
-    # # @purchased_listings (listings purchased by the user)
+    @active_listings = []
+    @sold_listings = []
+    # Iterate through @user.listings
+    @user.listings.each do |listing|
+      if listing.product_order
+        # if Listing has a product_order
+        # add it to @sold_listings.
+        @sold_listings.push listing
+      else
+        # Listing doesn't have a product order
+        # add it to @active_listings.
+        @active_listings.push listing
+      end
+    end
+
+    @purchased_listings = []
+    # Find product_orders where (buyer) user_id = @user.id.
+    @purchase_orders = ProductOrder.where(user_id: @user.id)
+    @purchase_orders.each do |order|
+      # Add each of them to @purchased_listings
+      @purchased_listings.push order.listing
+    end
+
   end
   
   # # PATCH/PUT /users/1
@@ -35,46 +53,10 @@ class UsersController < ApplicationController
     def set_user
       @user = User.find(params[:id])
     end
-
-    def set_users
-      @users = User.all
-    end
-
-    def set_listings
-      @listings = Listing.all
-    end
  
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       params.require(:user).permit(:role, :user_name, :picture)
-    end
-    
-    def set_active_listings
-      @active_listings = []
-      @user.listings.each do |listing|
-        unless listing.product_order.present?
-          @active_listings.push listing
-        end
-      end
-    end
-
-    def set_sold_listings
-      @sold_listings = []
-      @user.listings.each do |listing|
-        if listing.product_order
-          @sold_listings.push listing
-        end
-      end
-    end
-      
-    def set_purchased_listings
-      @purchased_listings = []
-      # Find product_orders where (buyer) user_id = @user.id.
-      @purchase_orders = ProductOrder.where(user_id: @user.id)
-      @purchase_orders.each do |order|
-        # Add each of them to @purchased_listings
-        @purchased_listings.push order.listing
-      end
     end
  
 end
