5d851f0e34615fc5b49f48c723e1068850616a79
Jim Farrugia <the,jim.farrugia@gmail.com>
2019-05-10 11:46:46 +1000

    added validation for custom fields in models/user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   has_many :listings
   has_one :locations_user
   has_one :location, through: :locations_user
   has_many :product_orders
   has_one_attached :picture
+
+  validates_presence_of     :username
+  validates_uniqueness_of   :username    
+  validates_presence_of     :phone_number
 end
